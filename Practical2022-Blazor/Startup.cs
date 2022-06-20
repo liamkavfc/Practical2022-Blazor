665fccfab5f9c916f230d8cd7e7d1a406f229f35
Liam Kenny <liamkavfc@live.co.uk>
2022-06-20 09:31:16 +0100

    remove left over boilerplate code

--- Practical2022-Blazor/Startup.cs
+++ Practical2022-Blazor/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Practical2022_Blazor.Api;
+﻿using Practical2022_Blazor.Api;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Practical2022_Blazor.Data;
 
 namespace Practical2022_Blazor
 {
